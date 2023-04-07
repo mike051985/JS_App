f9e4f252d58911b725a0c7c9d33fe9811ed80121
Michelet Saint-Preux <michelet2000@gmail.com>
2023-04-07 15:27:32 +0800

    index.html index.js style.css

--- index.js
+++ index.js
@@ -1,62 +1,70 @@
+// varaibles
+const taskList = document.getElementById("task-list");
+const addTaskForm = document.querySelector("form");
+const newTaskInput = document.getElementById("new-task");
+
+// Create an empty Array to add new Tasks
 let tasks = [];
 
-const app = document.getElementById("app");
-
-const form = document.createElement("form");
-const input = document.createElement("input");
-input.type = "text";
-input.placeholder = "Add a new task...";
-input.classList.add("task-input");
-
-const button = document.createElement("button");
-button.textContent = "Add";
-button.classList.add("btn", "btn-primary");
-
-form.appendChild(input);
-form.appendChild(button);
-app.appendChild(form);
-
-// Load any existing tasks from local storage
-if (localStorage.getItem("tasks")) {
-    //tasks = JSON.parse(localStorage.getItem("tasks"));
-    tasks.forEach((task) => {
-    renderTask(task);
-    });
+// Function to render the task list
+function renderTasks() {
+    // Clear the task list
+    taskList.innerHTML = "";
+    // Loop through the tasks and create list items for each one
+    for (let i = 0; i < tasks.length; i++) {
+        const task = tasks[i];
+        const li = document.createElement("li");
+        li.textContent = task.title;
+        // Add a class to the list item if it's completed
+        if (task.completed) {
+        li.classList.add("completed");
+        }
+        // Add event listeners for marking tasks as completed
+        li.addEventListener("click", function () {
+        task.completed = !task.completed;
+        li.classList.toggle("completed");
+        saveTasks();
+        });
+        
+        // Create a button for deleting tasks
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function () {
+        tasks.splice(i, 1);
+        saveTasks();
+        renderTasks();
+        });
+        // Add the list item and delete button to the task list
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+    }
 }
 
-form.addEventListener("submit", addTask);
-
-// Add Task
-function addTask(e) {
-    e.preventDefault();
-    const inputValue = input.value;
-    const newTask = {
-        id: Date.now(),
-        text: inputValue,
-    };
-    tasks.push(newTask);
-    renderTask(newTask);
+// Function to save tasks to local storage
+function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    input.value = "";
-    }
+}
 
-// Render Task
-function renderTask(task) {
-    const taskDiv = document.createElement("div");
-    taskDiv.classList.add("task");
-    taskDiv.dataset.id = task.id;
-    const taskText = document.createElement("span");
-    taskText.classList.add("task-text");
-    taskText.innerText = task.text;
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("btn", "btn-danger");
-    deleteBtn.innerText = "Delete";
-    deleteBtn.addEventListener("click", () => {
-        taskDiv.remove();
-        tasks = tasks.filter((t) => t.id !== task.id);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    });
-    taskDiv.appendChild(taskText);
-    taskDiv.appendChild(deleteBtn);
-    app.appendChild(taskDiv);
+// Load tasks from local storage on page load
+function loadTasks() {
+    const tasksJSON = localStorage.getItem("tasks");
+    if (tasksJSON !== null) {
+        tasks = JSON.parse(tasksJSON);
     }
+    renderTasks();
+}
+
+// Add event listener for adding tasks
+addTaskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const title = newTaskInput.value.trim();
+    if (title.length === 0) {
+        return;
+    }
+    tasks.push({ title: title, completed: false });
+    saveTasks();
+    newTaskInput.value = "";
+    renderTasks();
+});
+
+loadTasks();
